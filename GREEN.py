3fdb570a0263dc57da0932c7db8c64efb38aed3d
ᴍʀ.ᴋᴀᴜsᴀʀ‏ <ahkausar2@gmail.com>
2022-09-14 21:55:15 +0600

    Add files via upload

---
+++ GREEN.py
@@ -0,0 +1,25 @@
+import os, platform
+
+try:
+
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from KAUSAR_64 import m
+
+        m()
+
+elif bit == "32bit":
+
+        from KAUSAR_32 import m
+
+        m()
